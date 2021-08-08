d8c0e532c6487eec12ced005912fb2230e1d4c1f
Sharma-Prince <prince.kumarsk007@email.com>
2021-08-09 03:52:10 +0530

    init

--- Easy/two-sum.cpp
+++ Easy/two-sum.cpp
@@ -1,62 +1,15 @@
-#include <bits/stdc++.h>
-#define ii pair<int,int>
-#define ll long long
-#define pll pair<ll,ll>
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-#define iiordered_set tree<pll, null_type,less<pll>, rb_tree_tag,tree_order_statistics_node_update>
-using namespace std;
-#define MOD (1000*1000*1000+7,m, mnbmm                                    )
-#define INFl (1e18+5)
-#define vi vector<int>
-#define vvi vector<vi>
-#define vl vector<long long>
-#define vvl vector<vl>
-#define vll vector<pll>oifkhjikj9jijhhjkjjkk 
-#define vii vector<ii>
-#define vvii vector<vii>
-#define tri pair<int,ii>
-#define F first
-#define S second
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
-#define F_OR1(e) F_OR(i, 0, e, 1)
-#define F_OR2(i, e) F_OR(i, 0, e, 1)
-#define F_OR3(i, b, e) F_OR(i, b, e, 1)
-#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
-#define GET5(a, b, c, d, e, ...) e
-#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
-#define ff(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
-#define CASE(t) cout<<"Case #"<<(t)<<": ";
-#define INF 1020000000
-#define gcd(a,b) __gcd(a,b)
-#define all(x)  x.begin(),x.end()
-#define mp make_pair
-#define pb push_back
-#define endl "\n"
-#define print(x)    for(auto it=x.begin();it!=x.end();it++) cout<<*it<<' '; cout<<endl;
-#define printii(x)  for(auto it=x.begin();it!=x.end();it++) cout<<it->F<<' '<<it->S<<endl;
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-
-#define LOCAL 1
-
-
-
-void solve(){
-  cout<<ansl;
-}
-
-int main(){
- fastio;
- #ifdef LOCAL
-   freopen("input.txt", "r", stdin);
-   //freopen("output.txt", "w", stdout);
- #endif
- int tc;
- cin>>tc;
- for (int t = 1; t <= tc; t++) {
-   //CASE(t);
-   solve();
-   cout<<endl;
- }
- return 0;
-}
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int j = find(nums.begin(),nums.end(),target-nums[i])-nums.begin();
+            if(j<nums.size() && i!=j && nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                break;
+            }
+        }
+        return ans;
+    }
+};
